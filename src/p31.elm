6199257e7a69b13773e7dd963730d2166b43a339
John Crane <john.crane@gmail.com>
2017-12-11 20:51:29 -0800

    counting failed tests in p31. Added src directory to have pure elm files, these are not included in the gitbook.

--- src/p31.elm
+++ src/p31.elm
@@ -1,19 +1,28 @@
-# Problem 31
+module Main exposing (..)
 
-Determine whether a given integer number is prime.
-
-Example:
-```
-isPrime 113 == True 
-```
-## Unit Test
-```elm
 import Html
 
+
 isPrime : Int -> Bool
-isPrime n = 
-    -- your implementation goes here
-    False
+isPrime n =
+    if n < 2 then
+        False
+    else
+        eratos (abs n) (List.range 2 (n // 2))
+
+
+eratos : Int -> List Int -> Bool
+eratos n cs =
+    case cs of
+        [] ->
+            True
+
+        x :: xs ->
+            if n % x == 0 then
+                False
+            else
+                eratos n (List.filter (\y -> (y % x) /= 0) xs)
+
 
 main : Html.Html a
 main =
@@ -47,12 +56,3 @@ test =
             , ( isPrime 63247, True )
             , ( isPrime 63249, False )
             ]
-
-
-```
-
-## Hints
-1. Try the [Sieve of Eratosthenes](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes). ```List.filter``` will come in handy.
-
-## Solutions
-[Solutions](../s/s31.md)
